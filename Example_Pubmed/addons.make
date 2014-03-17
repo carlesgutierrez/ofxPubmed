02e0b4f25c57e8294353789d37a1c325852dffeb
carles <Admin@dude.local>
2014-03-17 01:14:20 +0100

    Addon structure

---
+++ Example_Pubmed/addons.make
@@ -0,0 +1,5 @@
+ofxXmlSettings
+ofxJSON
+ofxUI
+ofxFTGL
+ofxPubmed
---
+++ Example_Pubmed/src/ofApp.cpp
@@ -0,0 +1,86 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofLog(OF_LOG_VERBOSE, "guiPubMed");
+	ofSetLogLevel("guiPubMed", OF_LOG_VERBOSE); //check COUTS from Collisions
+	//ofLog(OF_LOG_VERBOSE, "xmlAbstract");
+	//ofSetLogLevel("xmlAbstract", OF_LOG_VERBOSE);
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	myPubMed.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	myPubMed.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+	myPubMed.keyPressed(key);
+	
+	if(key == OF_KEY_F1){
+		ofSetLogLevel(OF_LOG_SILENT);
+	}
+	else if(key == OF_KEY_F2){
+		ofSetLogLevel(OF_LOG_VERBOSE);
+	}
+	else if(key == OF_KEY_F3){
+		ofLog(OF_LOG_VERBOSE, "guiPubMed");
+		ofSetLogLevel("guiPubMed", OF_LOG_VERBOSE);
+	}
+	else if(key == OF_KEY_F4){
+		ofLog(OF_LOG_VERBOSE, "PubMed");
+		ofSetLogLevel("PubMed", OF_LOG_VERBOSE);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
